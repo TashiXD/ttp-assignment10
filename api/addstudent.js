4e6100b469c1a4fb9a524e55dd31c55e31b9e2cc
Tashi <tashishyangba23@gmail.com>
2023-06-26 22:14:01 -0400

    Added post function

---
+++ api/addstudent.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../db");
+
+router.use(express.json());
+//add new student into the database
+router.post("/", (req, res) => {
+    //extract data
+    const {student_name, major_id, college_id, year_id } = req.body;
+    console.log("Request body:", req.body);
+    console.log("Extracted values:", student_name, major_id, college_id, year_id);
+    //add an sql command to add the new student into the database
+    const sql = "INSERT INTO student (student_name, major_id, college_id, year_id) VALUES ($1, $2, $3, $4) returning *";
+    const values = [student_name, major_id, college_id, year_id];
+    db.query(sql, values,(err, result)=>{
+      try {
+        res.status(201).json({status: "success", data: result.rows[0]});
+      } catch (error) {
+        console.error('ERROR EXECUTING QUERY:', error);
+      }
+    })
+});
+
+module.exports = router;
--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 // Mount
 router.use("/getallstudent", require("./getallstudent"))
-
+router.use("/addstudent", require("./addstudent"));
 //handle 404
 router.use("*", (req, res, next)=>{
     const error = new Error('404 Not Found');
